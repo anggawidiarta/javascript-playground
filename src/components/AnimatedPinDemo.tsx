c32fc57dd99f82fadd0d2a92f9fe2823f42b88b6
Angga Widiarta <anggawidiarta55@gmail.com>
2024-03-16 00:40:32 +0800

    init push

---
+++ src/components/AnimatedPinDemo.tsx
@@ -0,0 +1,33 @@
+"use client";
+import React from "react";
+import { PinContainer } from "./ui/3d-pin";
+
+interface AnimatedPinDemoProps {
+  title?: string;
+  href?: string;
+  titleName?: string;
+  description?: string;
+}
+
+export function AnimatedPinDemo({
+  title = "/ui.aceternity.com",
+  href = "https://www.lipsum.com/",
+  titleName = "Lorem ipsum dolor ",
+  description = "The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax ",
+}: AnimatedPinDemoProps) {
+  return (
+    <div className="h-[25rem] w-fit flex items-center justify-center">
+      <PinContainer title={title} href={href}>
+        <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+          <h3 className="max-w-xs !pb-2 !m-0 font-bold capitalize  text-base text-slate-100">
+            {titleName}
+          </h3>
+          <div className="text-base !m-0 !p-0 font-normal">
+            <span className="text-slate-500 ">{description} </span>
+          </div>
+          <div className="flex flex-1 w-full mt-4 rounded-lg bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
+        </div>
+      </PinContainer>
+    </div>
+  );
+}
